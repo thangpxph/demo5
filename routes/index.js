077775864614926edcbe173258b117635b5f419d
phamthang <you@example.com>
2020-10-27 23:47:36 +0700

    login

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,22 @@
 var express = require('express');
 var router = express.Router();
+const User = require('../models/User');
 /* GET home page. */
-router.get('/', function (req, res, next) {
+router.get('/', function (req, res) {
     res.render('index');
-});
+})
+router.post('/', async function (req,res){
+    const username = req.body.username;
+    const password = req.body.password
+    const result = await User.findOne({username: username, password: password});
+    if (result !=null){
+        res.render('home');
+    } else {
+        res.render('index',{
+            status: "Thất Bại", content_status: "Tài khoản hoặc mật khẩu không chính xác",
+            isShowNotification: null,
+            styleButton:'alert alert-danger m-t-20'
+        })
+    }
+})
 module.exports = router;
