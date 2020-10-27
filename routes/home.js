1e9c65016452896260f118ab05559f69d25aa9bc
phamthang <you@example.com>
2020-10-27 22:38:54 +0700

    login

--- routes/home.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var router = express.Router();
-const User = require('../models/User');
-
-router.get('/home', async function (req, res, next){
-        res.render('home');
-});
-module.exports = router;
