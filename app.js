fe0455b2c9b811ef86278fe0d65382d6e2db030e
phamthang <you@example.com>
2020-11-11 18:54:57 +0700

    danh sach bàn

--- app.js
+++ app.js
@@ -30,7 +30,6 @@ app.engine('.hbs', hbs({
 }));
 app.set('view engine', 'hbs');
 
-app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static('public/images'));
