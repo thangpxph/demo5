90f996b24d5000f69ee9a0a9828f69a469033fa8
phamthang <you@example.com>
2020-11-11 18:44:59 +0700

    danh sach bàn

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var path = require('path');
 var hbs = require('express-handlebars')
-var logger = require('morgan');
 var mongoose = require('mongoose');
 var indexRouter = require('./routes/index');
 var homeRouter = require('./routes/home');
@@ -31,7 +30,6 @@ app.engine('.hbs', hbs({
 }));
 app.set('view engine', 'hbs');
 
-app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
